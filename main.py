1574b6cbc632b99805398213f996a164b8e49f9a
common-courtesy <thomas.cinalli45@gmail.com>
2025-07-12 19:43:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # or ["*"] for all origins during testing
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
